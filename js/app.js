9596c8964b866b7a8be7de9b4ade0115271c3955
EkaterinaTebyakina <ekaterina.tebyakina@yandex.ru>
2023-07-19 17:03:21 +0800

    did closing the opened slide

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ const sliderMain = new Swiper('.slider_main', {
     centeredSlides: true,
     mousewheel: true,
     parallax: true,
-    // slidesPerView: 3.5,
     breakpoints: {
         0: {
             slidesPerView: 2.5,
@@ -19,18 +18,48 @@ const sliderMain = new Swiper('.slider_main', {
 const sliderBg = new Swiper('.slider_bg', {
     centeredSlides: true,
     parallax: true,
+    allowTouchMove: false,
     slidesPerView: 3.5,
     spaceBetween: 60
 });
 
 sliderMain.controller.control = sliderBg;
-document.querySelectorAll('.slider_main .slider__item').forEach(item => {
-    item.addEventListener('click', event => {
-        item.classList.toggle('opened')
-    })
-})
 
 let desc = document.querySelector('.description')
 sliderMain.on('slideChange', e => {
     sliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden')
+})
+
+document.addEventListener('keydown', event => {
+
+    let sliderItem = document.querySelector('.opened');
+
+    if (event.key === 'Escape' && sliderItem) {
+        console.log('check')
+        sliderItem.classList.remove('opened');
+    }
+  }); 
+
+
+const slider = document.querySelector('.slider_main');
+
+document.addEventListener( 'click', (e) => {
+
+    const openedItem = document.querySelector('.opened');
+
+    if (openedItem) {
+
+        openedItem.classList.remove('opened'); 
+
+    } else {
+
+        const item = e.target.closest('.slider__item');
+
+        if (item && e.composedPath().includes(slider)) {
+                
+            item.classList.add('opened');
+        }
+    }
+
+	
 })
